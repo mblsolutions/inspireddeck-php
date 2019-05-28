7d37744538b04e2d1ec49e7b017434d3662ac039
Lerouse <christian.innerbichler@gmail.com>
2019-05-28 12:14:08 +0100

    add asset

---
+++ src/Asset.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace MBLSolutions\InspiredDeck;
+
+use MBLSolutions\InspiredDeck\Api\ApiResource;
+
+class Asset extends ApiResource
+{
+
+    /**
+     * View all Assets
+     *
+     * @param int|null $page
+     * @return array
+     */
+    public function all(int $page = null): array
+    {
+        return $this->getApiRequestor()->getRequest('/api/asset', [
+            'page' => $page
+        ]);
+    }
+
+    /**
+     * Create a new Asset
+     *
+     * @param array $params
+     * @return array
+     */
+    public function create(array $params): array
+    {
+        return $this->getApiRequestor()->postRequest('/api/asset', $params);
+    }
+
+    /**
+     * Update a Asset
+     *
+     * @param array $params
+     * @return array
+     */
+    public function update($id, array $params): array
+    {
+        return $this->getApiRequestor()->patchRequest('/api/asset/' . $id, $params);
+    }
+
+    /**
+     * Show a Asset
+     *
+     * @param $id
+     * @return array
+     */
+    public function show($id): array
+    {
+        return $this->getApiRequestor()->getRequest('/api/asset/' . $id);
+    }
+
+    /**
+     * Can Delete a Asset
+     *
+     * @param $id
+     * @return array
+     */
+    public function delete($id): array
+    {
+        return $this->getApiRequestor()->deleteRequest('/api/asset/' . $id);
+    }
+
+}
---
+++ tests/InspiredDeck/AssetTest.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace MBLSolutions\InspiredDeck;
+
+use MBLSolutions\TestCase;
+
+class AssetTest extends TestCase
+{
+    /** @var Asset $asset */
+    protected $asset;
+
+    /** {@inheritdoc} **/
+    public function setUp()
+    {
+        parent::setUp();
+
+        InspiredDeck::setToken('test_token');
+
+        $this->asset = new Asset();
+    }
+
+    /** @test **/
+    public function can_list_all()
+    {
+        $this->mockExpectedHttpResponse([
+            'data' => [
+                [
+                    'id' => 1,
+                    'name' => 'Test Asset',
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Test Asset 2',
+                ]
+            ]
+        ]);
+
+        $response = $this->asset->all();
+
+        $this->assertEquals($response, $this->getMockedResponseBody());
+    }
+
+    /** @test **/
+    public function can_create()
+    {
+        $this->mockExpectedHttpResponse([
+            'data' => [
+                'id' => 1,
+                'asset' => [],
+                'activation_rule' => [],
+                'expiration_rule' => [],
+                'sku' => 'TEST_ASSET',
+                'name' => 'Test Asset',
+                'bin' => 100123,
+                'algorithm' => 'luhn',
+                'type' => 'physical',
+                'reloadable' => true,
+                'single_use' => false
+            ]
+        ]);
+
+        $response = $this->asset->create([
+            'id' => 1,
+            'asset_id' => 1,
+            'activation_rule_id' => 1,
+            'expiration_rule_id' => 1,
+            'sku' => 'TEST_ASSET',
+            'name' => 'Test Asset',
+            'bin' => 100123,
+            'algorithm' => 'luhn',
+            'type' => 'physical',
+            'reloadable' => true,
+            'single_use' => false
+        ]);
+
+        $this->assertEquals($response, $this->getMockedResponseBody());
+    }
+
+    /** @test **/
+    public function can_update()
+    {
+        $this->mockExpectedHttpResponse([
+            'data' => [
+                'id' => 1,
+                'asset_id' => 1,
+                'activation_rule_id' => 1,
+                'expiration_rule_id' => 1,
+                'sku' => 'TEST_ASSET',
+                'name' => 'Test Asset',
+                'bin' => 100123,
+                'algorithm' => 'luhn',
+                'type' => 'physical',
+                'reloadable' => true,
+                'single_use' => false
+            ]
+        ]);
+
+        $response = $this->asset->update(1, [
+            'name' => 'Updated Asset',
+        ]);
+
+        $this->assertEquals($response, $this->getMockedResponseBody());
+    }
+
+    /** @test **/
+    public function can_delete()
+    {
+        $this->mockExpectedHttpResponse([
+            'data' => [
+                'id' => 1,
+                'asset_id' => 1,
+                'activation_rule_id' => 1,
+                'expiration_rule_id' => 1,
+                'sku' => 'TEST_ASSET',
+                'name' => 'Test Asset',
+                'bin' => 100123,
+                'algorithm' => 'luhn',
+                'type' => 'physical',
+                'reloadable' => true,
+                'single_use' => false
+            ]
+        ]);
+
+        $response = $this->asset->delete(1);
+
+        $this->assertEquals($response, $this->getMockedResponseBody());
+    }
+
+}
