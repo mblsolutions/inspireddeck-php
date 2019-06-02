240a8732881b546e27249b870b0d0570f2532605
Mal Boston <mal.boston1983@gmail.com>
2019-06-02 23:28:38 +0100

    Add User resource

---
+++ src/User.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MBLSolutions\InspiredDeck;
+
+use MBLSolutions\InspiredDeck\Api\ApiResource;
+
+class User extends ApiResource
+{
+
+    /**
+     * View all Users
+     *
+     * @param int|null $page
+     * @return array
+     */
+    public function all(int $page = null): array
+    {
+        return $this->getApiRequestor()->getRequest('/api/user', [
+            'page' => $page
+        ]);
+    }
+
+    /**
+     * Create a new User
+     *
+     * @param array $params
+     * @return array
+     */
+    public function create(array $params): array
+    {
+        return $this->getApiRequestor()->postRequest('/api/user', $params);
+    }
+
+    /**
+     * Update a User
+     *
+     * @param $id
+     * @param array $params
+     * @return array
+     */
+    public function update($id, array $params): array
+    {
+        return $this->getApiRequestor()->patchRequest('/api/user/' . $id, $params);
+    }
+
+    /**
+     * Show a User
+     *
+     * @param $id
+     * @return array
+     */
+    public function show($id): array
+    {
+        return $this->getApiRequestor()->getRequest('/api/user/' . $id);
+    }
+
+    /**
+     * Can Delete a User
+     *
+     * @param $id
+     * @return array
+     */
+    public function delete($id): array
+    {
+        return $this->getApiRequestor()->deleteRequest('/api/user/' . $id);
+    }
+
+}
